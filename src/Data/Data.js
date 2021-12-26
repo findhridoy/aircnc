d339f9ff5afbaa29d2a174a5146037a99995d4b6
findhridoy <find.hridoy546@gmail.com>
2021-12-26 16:13:16 +0600

    initialize aircnc app

---
+++ src/Data/Data.js
@@ -0,0 +1,214 @@
+import bImg1 from "../../Images/images/hotel-1.jpg";
+import bImg10 from "../../Images/images/hotel-10.jpg";
+import bImg11 from "../../Images/images/hotel-11.jpg";
+import bImg2 from "../../Images/images/hotel-2.jpg";
+import bImg3 from "../../Images/images/hotel-3.jpg";
+import bImg4 from "../../Images/images/hotel-4.jpg";
+import bImg5 from "../../Images/images/hotel-5.jpg";
+import bImg6 from "../../Images/images/hotel-6.jpg";
+import bImg7 from "../../Images/images/hotel-7.jpg";
+import bImg8 from "../../Images/images/hotel-8.jpg";
+import bImg9 from "../../Images/images/hotel-9.jpg";
+import thumb1 from "../../Images/images/hotel-out-1.jpg";
+import thumb10 from "../../Images/images/hotel-out-10.jpg";
+import thumb11 from "../../Images/images/hotel-out-11.jpg";
+import thumb2 from "../../Images/images/hotel-out-2.jpg";
+import thumb3 from "../../Images/images/hotel-out-3.jpg";
+import thumb4 from "../../Images/images/hotel-out-4.jpg";
+import thumb5 from "../../Images/images/hotel-out-5.jpg";
+import thumb6 from "../../Images/images/hotel-out-6.jpg";
+import thumb7 from "../../Images/images/hotel-out-7.jpg";
+import thumb8 from "../../Images/images/hotel-out-8.jpg";
+import thumb9 from "../../Images/images/hotel-out-9.jpg";
+
+const data = [
+  {
+    id: 1,
+    title: "Noise-free stay in Dhaka in an entire apartment E2",
+    description:
+      "Our place is a 1,000 sq feet fully​ furnished apartment at level six of a seven-storied building. It is located at west-Dhanmondi having a calm and quiet residential environment. You will love the open sky from your rooms and balconies. The rooftop is also allowed for guest and the entire complex is secured and friendly for any guest. The neighbors are educated and engaged in different professions. We will be happy to make your stay comfortable and you can reach us 24/7 over phone or email.",
+    moreDescription:
+      "We expect guests to share a copy of their photo ID (National ID or Passport or Social Card) during check-in. You can email the soft copy of the ID in advance so that the guard can identify you easily. This is required for all guests except a child under 18 years accompanied by parents. This is a mandatory process and we seek full cooperation from guests.",
+    price: 27,
+    rating: 4.7,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb1,
+      bImg: bImg1,
+    },
+  },
+  {
+    id: 2,
+    title: "Light bright airy stylish apt & safe peaceful stay",
+    description:
+      "It's newly constructed 7 storied building maintaining building code by a locally famous architect. Enough lights and natural air are playing here. The place (apartment) is calm and noise free. You'll love my place for its lovely and bright looks comfortable stay.Bangladesh is a beauty with its six seasons and green. The people are hospitable and worm.",
+    moreDescription:
+      "This space is near Dhanmondi lake, Buriganga river, National Museum, Louis kahn's Parliament House, Dhaka University, Lalbag fort etc.. Guest can access to Roof Top where they can spend time especially in the evening and morning to see the view of beautiful Dhaka, Bangladesh.",
+    price: 34,
+    rating: 4.7,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb2,
+      bImg: bImg2,
+    },
+  },
+  {
+    id: 3,
+    title: "Luxary apartment for rent for elite people.",
+    description:
+      "Our place is a 1,000 sq feet fully​ furnished apartment at level six of a seven-storied building. It is located at west-Dhanmondi having a calm and quiet residential environment. You will love the open sky from your rooms and balconies. The rooftop is also allowed for guest and the entire complex is secured and friendly for any guest. The neighbors are educated and engaged in different professions. We will be happy to make your stay comfortable and you can reach us 24/7 over phone or email.",
+    moreDescription:
+      "We expect guests to share a copy of their photo ID (National ID or Passport or Social Card) during check-in. You can email the soft copy of the ID in advance so that the guard can identify you easily. This is required for all guests except a child under 18 years accompanied by parents. This is a mandatory process and we seek full cooperation from guests.",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb3,
+      bImg: bImg3,
+    },
+  },
+  {
+    id: 4,
+    title: "Safe, Cozy Room near UN/World Bank",
+    description:
+      "This is a contemporary, comfortable and COVID-19 safe room in a serviced apartment for guests looking for both short-term and long-term stays. It’s ideal for those staying in quarantine lockdown. With fresh linen, towel, laundry, breakfast (on request) and daily house keeping, you are assured of a 5-star service. Fully equipped kitchen, spacious living space, super-fast WiFi internet, airport pick-up (on request & charged)",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows and great ventilation. Entire apartment is tastefully decorated with comfortable furnitures, lots of green indoor plants and beautiful wall paintings. Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb4,
+      bImg: bImg4,
+    },
+  },
+  {
+    id: 5,
+    title: "Airport Ac Studio room+private entrance",
+    description:
+      "My apartment is on the 2nd floor served by elevator/lift.A clean room with an airconditionar, a fridge,an infra-red cooker, a geyser, a tv, wifi connection and a tiny dining table etc. Our space is very new to our community in dhaka, we are offering a full furnished single room with a open kitchen, washroom, and a varanda.The whole Apt. is cozy and nicely decorated.we have attached kitchen so you can enjoy your own meal, also there is lots of Food options available around our place.",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows and great ventilation. Entire apartment is tastefully decorated with comfortable furnitures, lots of green indoor plants and beautiful wall paintings. Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb5,
+      bImg: bImg5,
+    },
+  },
+  {
+    id: 6,
+    title: "Three Room Luxury Serviced Apt",
+    description:
+      "This is a contemporary, stylish, comfortable and COVID-19 safe 3 bedroom serviced apartment for guests on both short-term and long-term stays. With complimentary breakfast (not available with discount rates), fresh linen, towel, laundry and regular house keeping, you are assured of a 5-star service. Fully equipped kitchen, spacious living space, WiFi internet, and three comfortable en-suite rooms with bathrooms and a large patio- this is the place for holiday, quarantine and business travelers.",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows, and great ventilation. The entire apartment is tastefully decorated with comfortable furniture, lots of green indoor plants, and beautiful wall paintings.Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb6,
+      bImg: bImg6,
+    },
+  },
+  {
+    id: 7,
+    title: "Lakeview condo",
+    description:
+      "This is a contemporary, stylish, comfortable and COVID-19 safe 3 bedroom serviced apartment for guests on both short-term and long-term stays. With complimentary breakfast (not available with discount rates), fresh linen, towel, laundry and regular house keeping, you are assured of a 5-star service. Fully equipped kitchen, spacious living space, WiFi internet, and three comfortable en-suite rooms with bathrooms and a large patio- this is the place for holiday, quarantine and business travelers.",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows, and great ventilation. The entire apartment is tastefully decorated with comfortable furniture, lots of green indoor plants, and beautiful wall paintings.Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb7,
+      bImg: bImg7,
+    },
+  },
+  {
+    id: 8,
+    title: "Smart, Safe Room w/Wifi",
+    description:
+      "This is a contemporary, stylish, comfortable and COVID-19 safe room in serviced apartment for guests looking for both short-term and long-term stays. Ideal for those staying in quarantine, or for holidays. With fresh linen, towel, laundry, breakfast (on request) and daily house keeping, you are assured of a 5-star service. Fully equipped kitchen, spacious living space, super-fast WiFi internet, airport pick-up (on request) and a very private and comfortable room with bathroom",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows, and great ventilation. The entire apartment is tastefully decorated with comfortable furniture, lots of green indoor plants, and beautiful wall paintings.Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb8,
+      bImg: bImg8,
+    },
+  },
+  {
+    id: 9,
+    title: "Small & Posh 2 Bedroom Apartment Rent",
+    description:
+      "The Apartment is in Gulshan-2 Residential Area. The apartment is fully furnished, air conditioned, peaceful, secured, modern, with full cooking amenities, attached bathrooms, WiFi, Work Desk etc. Gulshan-2 circle is 5 minutes walking distance. The place is very close to all types of restaurants, super market etc. Very quiet and secured area.",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows, and great ventilation. The entire apartment is tastefully decorated with comfortable furniture, lots of green indoor plants, and beautiful wall paintings.Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb9,
+      bImg: bImg9,
+    },
+  },
+  {
+    id: 10,
+    title: "Safe+Cozy Room+AC+WiFi",
+    description:
+      "A contemporary, comfortable and secured air-conditioned room in a serviced apartment for guests on both short-term and long-term stay. Complimentary breakfast (not provided with discount rates), fresh linen, towel, and daily house keeping, you are assured of a 5-star service. Its a quiet and safe neighborhood, within minutes from Diplomatic Zone, Gulshan, Banani and Airport. Its next to a supermarket, gym, restaurants and park for jogging. Airport Pickup and drop provided for a charge",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows, and great ventilation. The entire apartment is tastefully decorated with comfortable furniture, lots of green indoor plants, and beautiful wall paintings.Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb10,
+      bImg: bImg10,
+    },
+  },
+  {
+    id: 11,
+    title: "Light bright airy stylish apt & safe peaceful stay",
+    description:
+      "It's newly constructed 7 storied building maintaining building code by a locally famous architect. Enough lights and natural air are playing here. The place (apartment) is calm and noise free. You'll love my place for its lovely and bright looks comfortable stay. Bangladesh is a beauty with its six seasons and green. The people are hospitable and worm.",
+    moreDescription:
+      "This is a large apartment with lots of private and shared spaces, with sun decks, large bright windows, and great ventilation. The entire apartment is tastefully decorated with comfortable furniture, lots of green indoor plants, and beautiful wall paintings.Here is what you are getting!",
+    price: 36,
+    rating: 4,
+    guests: 3,
+    beds: 2,
+    baths: 2,
+    images: {
+      thumb: thumb11,
+      bImg: bImg11,
+    },
+  },
+];
+
+export default data;
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Components/Banner";
+import Header from "../Components/Header";
+
+function Home() {
+  return (
+    <div className="home">
+      <Header />
+      <Banner/>
+    </div>
+  );
+}
+
+export default Home;
